33613477ec63d1552c6a7503505e29a90baac5dd
Natália Simões <natalia.simoes.dev@gmail.com>
2022-06-18 18:11:29 -0300

    Show Placeholder when No Notes

--- src/views/NoteViews.vue
+++ src/views/NoteViews.vue
@@ -11,6 +11,9 @@
     
     <template v-else>
       <Note :note="note" v-for="note in storeNotes.notes" :key="note.id" />
+      <div v-if="!storeNotes.notes.length" class="is-size-4 has-text-centered has-text-grey-light is-family-monospace py-6">
+        No notes here yet...
+      </div>
     </template>
   </div>
 </template>
